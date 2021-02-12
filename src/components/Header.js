e44b9e4e381d8f1d95607c7285766fb987337f3a
KB <katrinkajb@gmail.com>
2021-02-11 16:08:34 -0800

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react'
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                <header className='header-title'>Pokedex</header>
-                <div className='links'>
-                    <a href='./HomePage.js'>Home</a>
-                    <a href='./SearchPage.js'>Search</a>
-                </div>
+            <div className='Header'>
+                <header className='header-title'>Pokedex
+                    <div className='links'>
+                        <a href='./HomePage.js'>Home</a> 
+                        <a href='./SearchPage.js'>Search</a>
+                    </div>
+                </header>
             </div>
         )
     }
