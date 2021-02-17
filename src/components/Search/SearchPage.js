d7b8a03a24463962c60b8fb8e721f37a27833da6
KB <katrinkajb@gmail.com>
2021-02-16 17:53:34 -0800

    added spinner and sweet Drake gif

--- src/components/Search/SearchPage.js
+++ src/components/Search/SearchPage.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import '../../App.css';
 import PokeList from './PokeList.js';
 import SearchBar from './SearchBar.js';
-// import pokeData from '../../data.js';
 import SortOrder from './SortOrder';
 import request from 'superagent';
-
+import LoadingSpinner from './LoadingSpinner.js';
 
 export default class SearchPage extends Component {
     state = {
@@ -13,6 +12,7 @@ export default class SearchPage extends Component {
         sortBy: 'pokemon',
         order: 'asc',
         searchQuery: '',
+        loading: false,
     }
 
     componentDidMount = async () => {
@@ -20,10 +20,15 @@ export default class SearchPage extends Component {
       }
 
     fetchPokemon = async () => {
+        this.setState({
+            loading: true
+        })
+        
         const data = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}&sort=${this.state.sortBy}&direction=${this.state.order}`);
         
         this.setState({     
             pokemon: data.body.results,
+            loading: false,
           })
     }
 
@@ -70,8 +75,11 @@ export default class SearchPage extends Component {
                             handleOrderChange={this.handleOrderChange}
                         />
                     </div>
+                    <img className='drake' width='220' height='220' alt='Drake with pokeballs' src='https://media.giphy.com/media/vsyKKf1t22nmw/giphy.gif' />
+
                 </div>
                 <div className='main-area'>
+                    { this.state.loading && <LoadingSpinner /> }
                     <PokeList 
                         pokeData={this.state.pokemon}
                     />
